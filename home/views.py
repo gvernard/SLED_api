b6f8a32e17e908a8823b4f7cf43948d741ee6e2b
gvernard <gvernard@astro.rug.nl>
2020-12-05 18:57:12 +0100

    Testing the git interface with the server.

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. Time to party.")
+    return HttpResponse("Hello, world. Time to party, yeah!")
 
 
