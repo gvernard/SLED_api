06bf277c3586eecde81b83a0324a9af454eb383e
Cameron Lemon <cameron.lemon@epfl.ch>
2021-06-29 17:39:51 +0200

    website progress

--- tests/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Users
-from .models import Lenses
-
-admin.site.register(Users, UserAdmin)
-admin.site.register(Lenses)
--- tests/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LensdbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lensdb'
