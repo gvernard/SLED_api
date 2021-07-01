698e015927d5ada811ea7e3e1f038818afe77cc9
gvernard <gvernard@astro.rug.nl>
2021-07-01 19:03:37 +0200

    Fixed the previous problem. Notification system is in place now.

--- tests/basic_tests/send_notification.py
+++ tests/basic_tests/send_notification.py
@@ -1,10 +1,21 @@
+import os
+import sys
 import django
 
 base_dir = '../../'
 sys.path.append(base_dir)
 
+#Database init
+os.environ['DJANGO_SETTINGS_MODULE'] = "mysite.settings"
+django.setup()
+
+# -----------------------------------------------------
+
+
 from lenses.models import Users, SledGroups, Lenses
 from notifications.signals import notify
 
+actor = Users.objects.get(username='Cameron')
 
-notify.send(user, recipient=user, verb='you reached level 10')
+#print('hello')
+notify.send(actor, recipient=actor, verb='you reached level 10')
