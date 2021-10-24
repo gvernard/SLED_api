81f4d31b7e27c3c67c560594d3a0a87f421cab2e
gvernard <gvernard@astro.rug.nl>
2021-10-24 17:34:49 +0200

    Added confirmation tasks link to the user profile page.

--- confirmation/urls.py
+++ confirmation/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, re_path
 
 from . import views
 
+app_name = 'confirmation'
 urlpatterns = [
-    path('', views.list_tasks, name='list_tasks'),
-    re_path('single/(?P<task_id>[A-Za-z0-9\w|\W\- ]+)/$', views.single_task, name='single_task'),
+    path('', views.list_tasks, name='list-tasks'),
+    re_path('single/(?P<task_id>[A-Za-z0-9\w|\W\- ]+)/$', views.single_task, name='single-task'),
 ]
