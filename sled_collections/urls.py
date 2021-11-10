3d48e99575d5623505bc13287f29f304bf7065ab
gvernard <gvernard@astro.rug.nl>
2021-11-10 17:22:33 +0100

    Added generic classes to collection views.

--- sled_collections/urls.py
+++ sled_collections/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 from django.views.generic.base import RedirectView
-from . import views
+from sled_collections.views import CollectionDetailView, CollectionListView
 
 app_name = 'sled_collections'
 urlpatterns = [
-    path('', views.list_collections, name='list-collections'),
-    path('single/',RedirectView.as_view(url='/sled_collections/',permanent=False),name='redirect-single'),
-    re_path('single/(?P<collection_id>[A-Za-z0-9\w|\W\- ]+)/$', views.single_collection, name='single-collection'),
+    path('', CollectionListView.as_view(), name='collections-list'),
+    path('detail/',RedirectView.as_view(url='/sled_collections/',permanent=False),name='redirect-detail'),
+    path('detail/<int:pk>/', CollectionDetailView.as_view(), name='collections-detail'),
 ]
