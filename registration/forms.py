eb84b3e31cfe5cb3454d5f1f4c90f65a92d72ffe
Cameron Lemon <cameron.lemon@epfl.ch>
2021-12-16 16:58:28 +0100

    Fixing the login and register pages for Justin to pull; changes were specifically made to the forms.py and views.py in the registration app, the urls.py in mysite/, and the templates login.html and register.html in the main templates folder

--- registration/forms.py
+++ registration/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from lenses.models import Users
-from django.contrib.auth.forms import UserCreationForm
+from lenses.models.user import Users
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
-
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +9,26 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = Users
         fields = ["username", "email", "password1", "password2", "affiliation"]
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        # self.fields['username'].widgets.attrs.update({'class', 'field-label'})
+        self.fields['username'].widget.attrs['class'] = 'field-label'
+        self.fields['email'].widget.attrs['class'] = 'field-label'
+        self.fields['password1'].widget.attrs['class'] = 'field-label'
+        self.fields['password2'].widget.attrs['class'] = 'field-label'
+        self.fields['affiliation'].widget.attrs['class'] = 'field-label'
+
+
+
+class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = Users
+        fields = ["username", "password"]
+
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'field-label'
+        self.fields['password'].widget.attrs['class'] = 'field-label'
--- registration/views.py
+++ registration/views.py
@@ -1,19 +1,39 @@
 # views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .forms import RegisterForm
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import RegisterForm, UserLoginForm
 
 
 # Create your views here.
 def register(response):
     #if someone is posting their form data, redirect them to (home for now)
     if response.method == "POST":
+        print('SOMEONE IS TRYING TO REGISTER')
         form = RegisterForm(response.POST)
-        print(form)
         if form.is_valid():
             form.save()
-            return redirect("../")
+            print('form validated')
+            return HttpResponseRedirect("../login/")
     else:
+        print('someone is on the register page')
         form = RegisterForm()
 
     return render(response, "register.html", {"form":form}) 
+
+
+# Create your views here.
+def login(response):
+    #if someone is posting their form data, redirect them to (home for now)
+    if response.method == "POST":
+        print('SOMEONE IS TRYING TO LOGIN')
+        form = UserLoginForm(response.POST)
+        if form.is_valid():
+            form.save()
+            print('form validated')
+            return redirect('../') #render(response, "login.html", {"form":form}) #HttpResponseRedirect("../")
+    else:
+        print('someone is on the login page')
+        form = UserLoginForm()
+
+    return render(response, "login.html", {"form":form}) 
