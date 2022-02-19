e4de267ccb1edc2f295f41b7db9d4e311c71def4
gvernard <gvernard@astro.rug.nl>
2022-02-19 22:20:49 +0100

    Remove old create collection form.

--- sled_collections/forms.py
+++ sled_collections/forms.py
@@ -4,62 +4,7 @@ from lenses.models import Collection, Lenses, Users, SledGroup
 from django.apps import apps
 from bootstrap_modal_forms.forms import BSModalModelForm,BSModalForm
 
-
-class CustomM2M(forms.ModelMultipleChoiceField):
-    def label_from_instance(self,item):
-        #return "%s" % item.get_absolute_url()
-        return "%s" % item.__str__()
-
-    def clean(self,item):
-        return(item)
-
-    
-class CollectionCreateForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user',None)
-        self.request = kwargs.pop('request',None)
-        super(CollectionForm,self).__init__(*args, **kwargs)
-        data = kwargs.get('data')
-        obj_type = data['obj_type']
-        ids = data['all_items'].split(',')
-        qset = apps.get_model(app_label='lenses',model_name=obj_type).accessible_objects.in_ids(self.user,ids)
-        self.fields["myitems"].queryset = qset
-
-    class Meta:
-        model = Collection
-        fields = ['name','description']
-        widgets = {
-            'description': forms.Textarea({'placeholder':'Provide a description for your collection.','rows':3,'cols':30})
-        }
-
-    obj_type = forms.CharField(widget=forms.HiddenInput)
-        
-    # Need to define the field because gm2m has not a default input type
-    myitems = CustomM2M(
-        queryset=None,
-        widget=forms.CheckboxSelectMultiple(attrs={'checked':'checked'})
-    )
-    all_items = forms.CharField(widget=forms.HiddenInput)
-
-    def clean(self):
-        cleaned_data = super(CollectionForm,self).clean()
-        
-        ### Check if form has changed.
-        if not self.has_changed():
-            raise ValidationError("No changes detected.")
-
-        if 'myitems' in cleaned_data:
-            if len(cleaned_data['myitems']) <= 1:
-                self.add_error('myitems','More than one object required')
-        else:
-            self.add_error('myitems','More than one object required')
-
-
-
-
-
-
-            
+          
 class CollectionUpdateForm(BSModalModelForm):
     class Meta:
         model = Collection
