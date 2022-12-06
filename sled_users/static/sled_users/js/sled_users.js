86cf83a9ecfd368693fb53b996bbcaa4091b2d10
gvernard <gvernard@astro.rug.nl>
2022-12-06 17:09:48 +0100

    Added accordion menu to the user's profile page.

---
+++ sled_users/static/sled_users/js/sled_users.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    $('.p').hide();
+    
+    $(".sled-process-lenses").click(function() {
+	// Construct get query string
+	var values = [];
+	$('#exe_summary input[type="checkbox"]:checked').each(function() {
+            values.push('ids=' + $(this).val());
+	});
+
+	if (values.length == 0) {
+            alert('You need to select at least one item!');
+	} else {
+            var get_str = '?' + values.join('&') + '&';
+
+            // Fetch only the first part of the URL (without any GET arguments)
+            var url = $(this).data('form-url').split('?');
+            var url_core = url[0];
+
+            // Trigger modal
+            var new_form_url = url_core + get_str;
+            $(this).modalFormTrigger({
+		formURL: new_form_url,
+		modalID: "#id-modal"
+            });
+	}
+    });
+
+    // Modals
+    $(".sled-modal").each(function() {
+	$(this).modalForm({
+            formURL: $(this).data("form-url"),
+            modalID: "#id-modal"
+	});
+    });
+    
+    // Accordion
+    $('input[type=checkbox]').click(function(){
+	$(this).siblings('div').animate({
+	    height: "toggle"
+	}, 500, function() {
+	    // Animation complete.
+	});
+    });
+});
