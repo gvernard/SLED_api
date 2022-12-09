aebb16df23baac99436607cf748993ca3f08c154
gvernard <gvernard@astro.rug.nl>
2022-12-09 19:26:39 +0100

    Fixed some little issues with the page to add lenses.

--- lenses/static/lenses/js/lens_add.js
+++ lenses/static/lenses/js/lens_add.js
@@ -12,11 +12,17 @@ $( document ).ready(function() {
 	addContainerClass: "sled-add-button-hidden",
 	prefix: prefix
     });
+
+
+    $delete = $(".delete-row")
+    $delete.bindFirst('click',function(e,next){
+	$(this).closest('.sled-row-content').slideUp(1000);
+	setTimeout(next, 1000); // holds the other handlers for 1sec
+    });
 });
 
 function reset_counter(row){
     $(".sled-lens-counter").each(function(index,element){
-	//row.find('.my-select2').select2('destroy'); 
 	$(element).text('Lens '+(index+1).toString());
 	console.log((index+1).toString());
     });
@@ -24,15 +30,41 @@ function reset_counter(row){
 }
 
 function reposition(form_div){
-    form_div.detach().appendTo('#sled-lenses-table');
+    form_div.detach();
+    form_div.children('td').children('div').hide();
+    form_div.appendTo('#sled-lenses-table');
+
+    $delete = form_div.find(".delete-row")
+    $delete.bindFirst('click',function(e,next){
+	$(this).closest('.sled-row-content').slideUp(1000);
+	setTimeout(next, 1000); // holds the other handlers for 1sec
+    });
+
     form_div.find('.my-select2').each(function(index,element){
-	$(element).attr('multiple','multiple');
-	$(element).find('option')[0].remove();
+	$(element).select2();
     });
-    $('.my-select2').select2({
-	multiple: true,
-	placeholder: 'Select an option'
-    });
+
     counter = counter + 1;
     form_div.find('.sled-lens-counter').text('Lens '+counter.toString());
+
+    form_div.children('td').children('div').slideDown(1000);
 }
+
+$.fn.bindFirst = function(type, handler) {
+    return this.each(function() {
+        var elm = this;
+        var evs = $._data(this).events;
+        if ( type in evs ) {
+            var handlers = evs[type].map(function(ev) {
+                return ev.handler;
+            });
+            $(elm).unbind(type).on(type, function(e) {
+                handler.call(elm, e, function() {
+                    handlers.forEach(function(fn) {
+                        fn.call(elm);
+                    });
+                });
+            });
+        }
+    });
+};
