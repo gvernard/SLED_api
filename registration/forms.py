2da089ca12895b2ff68b15110f25f53fc11a307f
cameronalemon <cameron.lemon@epfl.ch>
2023-02-02 11:27:29 +0100

    Made all fields required on registration form

--- registration/forms.py
+++ registration/forms.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
 
     class Meta:
         model = Users
