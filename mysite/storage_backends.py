d25027dbdc0f14312a87048e71ceece7874a8ec9
Giorgos <gvernardos@amnh.org>
2024-03-13 19:19:05 -0400

    Adding custom file storage.

--- mysite/storage_backends.py
+++ mysite/storage_backends.py
@@ -12,7 +12,13 @@ class DatabaseFileStorage(S3Boto3Storage):
     default_acl = 'public-read'
     file_overwrite = False
 
+    def _normalize_name(self, name):
+        try:
+            return safe_join(self.location, name).lstrip('/')
+        except ValueError:
+            raise SuspiciousOperation("Attempted access to '%s' denied." % name)
 
+        
     def copy(self,from_path,to_path):
         print(from_path,to_path)
         from_path = clean_name(from_path)
