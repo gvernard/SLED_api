7da20a3a5cec75f79193beaf92427bbedb6924f3
Giorgos <gvernardos@amnh.org>
2024-03-13 19:21:41 -0400

    Adding custom file storage.

--- mysite/storage_backends.py
+++ mysite/storage_backends.py
@@ -2,6 +2,36 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 from storages.utils import clean_name
 
+
+def safe_join(base, *paths):
+    """
+    A version of django.utils._os.safe_join for S3 paths.
+
+    Joins one or more path components to the base path component intelligently.
+    Returns a normalized version of the final path.
+
+    The final path must be located inside of the base path component (otherwise
+    a ValueError is raised).
+
+    Paths outside the base path indicate a possible security sensitive operation.
+    """
+    from urlparse import urljoin
+    base_path = force_unicode(base)
+    paths = map(lambda p: force_unicode(p), paths)
+    final_path = urljoin(base_path + ("/" if not base_path.endswith("/") else ""), *paths)
+    # Ensure final_path starts with base_path and that the next character after
+    # the final path is '/' (or nothing, in which case final_path must be
+    # equal to base_path).
+    base_path_len = len(base_path) - 1
+    if not final_path.startswith(base_path) \
+       or final_path[base_path_len:base_path_len + 1] not in ('', '/'):
+        raise ValueError('the joined path is located outside of the base path'
+                         ' component')
+    return final_path
+
+
+
+
 class StaticStorage(S3Boto3Storage):
     location = settings.STATIC_LOCATION
     default_acl = 'public-read'
@@ -11,6 +41,7 @@ class DatabaseFileStorage(S3Boto3Storage):
     location = settings.DATABASE_FILE_LOCATION
     default_acl = 'public-read'
     file_overwrite = False
+
 
     def _normalize_name(self, name):
         try:
