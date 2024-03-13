13e73618e4fc4fff2c0046d6114bd86bf2fca745
Giorgos <gvernardos@amnh.org>
2024-03-13 19:15:04 -0400

    Adding custom file storage.

--- mysite/storage_backends.py
+++ mysite/storage_backends.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-
+from storages.utils import clean_name
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATIC_LOCATION
@@ -15,8 +15,9 @@ class DatabaseFileStorage(S3Boto3Storage):
 
     def copy(self,from_path,to_path):
         print(from_path,to_path)
-        from_path = self._normalize_name(self._clean_name(self.location + from_path))
-        to_path = self._normalize_name(self._clean_name(self.location + to_path))
+        from_path = self._normalize_name(clean_name(self.location + from_path))
+        to_path = self._normalize_name(clean_name(self.location + to_path))
+        print(from_path,to_path)
         
         copy_result = self.connection.meta.client.copy_object(
             Bucket=self.bucket_name,
