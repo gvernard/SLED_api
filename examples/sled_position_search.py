a58c49b5854eede6dc25319ba47424e1570eebfe
Cameron Lemon <cameron@eduroam-10-200-1-254.local.su.se>
2024-05-02 14:18:07 +0200

    removed explicit password

--- examples/sled_position_search.py
+++ examples/sled_position_search.py
@@ -6,6 +6,6 @@ urlquery = "http://127.0.0.1:8000/api/query-lenses/"
 ra, dec = 260.45419, 88.70621
 
 lensdata = {'ra':ra, 'dec':dec, 'radius':10.}
-r = requests.post(urlquery, data=lensdata, auth=HTTPBasicAuth('Cameron','123'))
+r = requests.post(urlquery, data=lensdata, auth=HTTPBasicAuth(username, password))
 dbquery = json.loads(r.text)
 
