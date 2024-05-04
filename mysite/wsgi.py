2e0cc63edd891d339fe574b39b84d7bf8358cbfa
Giorgos <gvernardos@amnh.org>
2024-05-03 22:26:13 -0400

    Wsgi.py now works.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -29,11 +29,21 @@ def application(environ, start_response):
     os.environ['S3_SECRET_ACCESS_KEY'] = environ['S3_SECRET_ACCESS_KEY']
     os.environ['S3_STORAGE_BUCKET_NAME'] = environ['S3_STORAGE_BUCKET_NAME']
     os.environ['S3_ENDPOINT_URL'] = environ['S3_ENDPOINT_URL']
+    os.environ['DJANGO_NO_LAST_LOGIN'] = 'true'
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.append(BASE_DIR)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-    django.setup()
     get_wsgi_application()
-    #return super(WSGIEnvironment, self).__call__(environ, start_response)
+
+    
+'''
+import os
+def application(environ, start_response):
+    if environ['mod_wsgi.process_group'] != '': 
+        import signal
+        os.kill(os.getpid(), signal.SIGINT)
+    return ["killed"]
+
+'''
