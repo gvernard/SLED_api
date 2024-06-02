e04abe8fdfbbcb780315f79d70999c59e9131c15
Giorgos <gvernardos@amnh.org>
2024-06-02 19:30:02 -0400

    Temporary CSP implementation - problem with typekit script.

---
+++ static/js/load_fonts.js
@@ -0,0 +1,8 @@
+WebFont.load({
+    google: {
+        families: ["Basic:regular"]
+    }
+});
+try {
+    Typekit.load();
+} catch (e) {}
---
+++ static/js/message_banner.js
@@ -0,0 +1,5 @@
+$("#alert-message").fadeTo(2000,500).slideUp(500,function(){
+    $("#alert-message").slideUp(500);
+});
+
+global_search_autocomplete('global_search');
