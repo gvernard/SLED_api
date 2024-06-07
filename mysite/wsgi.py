0a0653fa87df40698aeaba2c96b19270079456df
Giorgos <gvernardos@amnh.org>
2024-06-07 13:46:20 -0400

    Using 'load_dotenv' in wsgi.py.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,27 +1,21 @@
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-
+from dotenv import load_dotenv
 
 class Application(object):
 
     def __call__(self, environ, start_response):
 
-        os.environ['SLED_PROD'] = environ['SLED_PROD']
-        if os.environ['SLED_PROD'] == 'active':
-            os.environ['S3_ENDPOINT_URL'] = environ['S3_ENDPOINT_URL']
-            os.environ['DJANGO_SLACK_API_TOKEN'] = environ['DJANGO_SLACK_API_TOKEN']
-            os.environ['DJANGO_EMAIL_PASSWORD'] = environ['DJANGO_EMAIL_PASSWORD']
-            os.environ['DJANGO_DB_FILE'] = environ['DJANGO_DB_FILE']
-            os.environ['DJANGO_SECRET_KEY'] = environ['DJANGO_SECRET_KEY']
-            os.environ['S3_ACCESS_KEY_ID'] = environ['S3_ACCESS_KEY_ID']
-            os.environ['S3_SECRET_ACCESS_KEY'] = environ['S3_SECRET_ACCESS_KEY']
-            os.environ['S3_STORAGE_BUCKET_NAME'] = environ['S3_STORAGE_BUCKET_NAME']
-            os.environ['S3_ENDPOINT_URL'] = environ['S3_ENDPOINT_URL']
-            os.environ['DJANGO_NO_LAST_LOGIN'] = 'true'
-
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         sys.path.append(BASE_DIR)
+
+        if os.path.isfile('../.env'):
+            load_dotenv(dotenv_path)
+            print('Loading .env file.')
+        else:
+            print('Environment .env file not found.')
+        
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
         _application = get_wsgi_application()
