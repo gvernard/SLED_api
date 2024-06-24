6288f864fa476644159bbb0c9885b68a74777d1a
Giorgos <gvernardos@amnh.org>
2024-06-24 17:16:47 -0400

    Final redshift script.

--- scripts/get_redshifts.py
+++ scripts/get_redshifts.py
@@ -14,11 +14,21 @@ sys.path.append(dirname)
 os.environ['DJANGO_SETTINGS_MODULE'] = "mysite.settings"
 django.setup()
 
-from lenses.models import Lenses
 
 
+from lenses.models import Lenses, Users, Redshift
 
-user = Users.get(username='Giorgos')
 
-lenses = Lenses.accessible_objects.all(user).filter(flag='Confirmed')
-print(len(lenses))
+path = '/var/www/sled/SLED_api/scripts/'
+
+
+user = Users.objects.get(username='Giorgos')
+
+seps = Lenses.objects.filter(flag='CONFIRMED').filter(image_sep__gt=0.01).values_list('image_sep',flat=True)
+np.savetxt(path+'separations.dat',seps)
+
+z_lens = Redshift.objects.filter(tag='LENS').filter(lens__flag='CONFIRMED').values_list('value',flat=True)
+np.savetxt(path+'z_lens.dat',z_lens)
+
+z_source = Redshift.objects.filter(tag='SOURCE').filter(lens__flag='CONFIRMED').values_list('value',flat=True)
+np.savetxt(path+'z_source.dat',z_source)
